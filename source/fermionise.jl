f9a9b32c67e0bbb41011b9b57cfaecd1d17f5669
SearyBlue <mukherjeeabhirup44@gmail.com>
2024-01-27 21:41:18 +0530

    final Julia version of getting Hamiltonian from operator

--- source/fermionise.jl
+++ source/fermionise.jl
@@ -1,17 +1,15 @@
 using Combinatorics
 using Distributed
-using LinearAlgebra
-using SharedArrays
-@everywhere using ProgressMeter
-@everywhere using SharedArrays, Distributed
+using ProgressMeter
+using SparseArrays
 
-function BasisStates(numLevels::Int, totOccupancy=-999, totSpin=-999)
-	basisStates = String[]
+
+function BasisStates(numLevels, totOccupancy=-999, totSpin=-999)
+	basisStates = []
 	for i in 0:2^(numLevels)-1
-		state = lpad(string(i, base=2), numLevels, '0')
-        stateTotOccupancy = sum([parse(Int, ch) for ch in state])
-        stateTotSpin = 0.5 * (sum([parse(Int, ch) for ch in state[1:2:end]]) 
-            - sum([parse(Int, ch) for ch in state[2:2:end]]))
+		state = [parse(Int, ch) for ch in lpad(string(i, base=2), numLevels, '0')]
+        stateTotOccupancy = sum(state)
+        stateTotSpin = 0.5 * (sum(state[1:2:end]) - sum(state[2:2:end]))
 		if ((totOccupancy == -999 || stateTotOccupancy == totOccupancy)
             && (totSpin == -999 || stateTotSpin == totSpin))
 			push!(basisStates, state)
@@ -21,116 +19,40 @@ function BasisStates(numLevels::Int, totOccupancy=-999, totSpin=-999)
 end
 
 
-function InnerProduct(genStateLeft::Dict, genStateRight::Dict)
-	innerProduct = sum([conj(genStateLeft[key]) * genStateRight[key] for (key, value) in genStateRight if haskey(genStateLeft, key)])
-    return innerProduct
-end
-
-
-function ComputationalCoefficients(basisStates::Vector{String}, genState)
-    @assert length(basisStates) == length(genState)
-    stateDecompose = Dict([(basisStates[i], coeff) for (i, coeff) in enumerate(genState) if abs(coeff) > 0])
-    return stateDecompose
-end
-
-
-function MatrixElement(leftState::Dict{String, Float64}, operatorList, rightState::Dict{String, Float64})
-    return InnerProduct(leftState, ApplyOperatorOnState((rightState, operatorList))) 
-end
-
-
-@everywhere function ApplyChunkOnBasisState(basisState::String, operatorChunk::String, siteIndices)
-    @assert ! (false in [k in ['+', '-', 'n', 'h'] for k in operatorChunk]) "Interaction type not among +, - or n."
-    @assert length(operatorChunk) == length(siteIndices) "Number of site indices in term does not match number of provided interaction types."
-
-    prefactor::Float64 = 1
-    basisStateArray = [parse(Int, ch) for ch in split(basisState, "")]
-    for (op, index) in zip(reverse(operatorChunk), reverse(siteIndices))
-    	siteOccupancy = basisStateArray[index]
-        if op == 'n'
-            prefactor *= siteOccupancy
-    	elseif op == 'h'
-            prefactor *= 1 - siteOccupancy
-    	elseif (op == '+' && siteOccupancy == 1) || (op == '-' && siteOccupancy == 0)
-            prefactor = 0
-        else
-    	    prefactor *= index == 1 ? 1 : (-1) ^ cumsum(basisStateArray)[index-1]
-    	    basisStateArray[index] = 1 - siteOccupancy
-        end
-        if prefactor == 0
-            break
-        end
+function TransformQubit(qubit, operator)
+    if operator == 'n'
+        return qubit, qubit
+    elseif operator == 'h'
+        return qubit, 1 - qubit
+    elseif (operator == '+' && qubit == 0) || (operator == '-' && qubit == 1)
+        return 1 - qubit, 1
+    else
+        return qubit, 0
     end
-    return join(basisStateArray), prefactor
-end
-
-
-function diagonalise(basisStates, matrix)
-    if length(basisStates) == 0
-        println("Chosen basis is empty, bailing out.")
-        return
-    end
-    F = eigen(Hermitian(matrix), 1:2)
-    eigenstateDecompositionArray = [ComputationalCoefficients(basisStates, eigenvector) for eigenvector in eachcol(F.vectors)]
-    return F.values, eigenstateDecompositionArray
-end
-
-
-@everywhere function ApplyOperatorOnState(args)
-    genState, operatorList = args
-	finalState = Dict()
-	for (basisState, basisCoefficient) in collect(genState)
-		for (couplingStrength, operatorChunk, siteIndices) in operatorList
-			modifiedBasisState, prefactor = ApplyChunkOnBasisState(basisState, operatorChunk, siteIndices)
-			if haskey(finalState, modifiedBasisState)
-				finalState[modifiedBasisState] += prefactor * basisCoefficient * couplingStrength
-			else
-				finalState[modifiedBasisState] = prefactor * basisCoefficient * couplingStrength
-			end
-		end
-	end
-	return finalState
-end
-
-@everywhere function helper(args)
-    (startIndex, basisStates, operatorList, lengthBasis) = args
-    columnVector = zeros(lengthBasis)
-    for operator in operatorList
-        (couplingStrength, operatorChunk, siteIndices) = operator
-        endState, prefactor = ApplyChunkOnBasisState(basisStates[startIndex], operatorChunk, siteIndices)
-        if prefactor != 0
-            endIndex = findall(basisStates .== endState)[1]
-            columnVector[endIndex] += couplingStrength * prefactor
-        end
-    end
-    return columnVector
 end
 
 
 function GeneralOperatorMatrix(basisStates, operatorList, show_progress=true)
-	lengthBasis = length(basisStates)
-    argsList = [(startIndex, basisStates, operatorList, lengthBasis) for startIndex in 1:lengthBasis]
-    generalOperatorMatrix = stack(@showprogress enabled=show_progress pmap(helper, argsList, batch_size=10))
-	return generalOperatorMatrix
-end
-
-
-function ComputeCorrelations(basisStates, hamiltonianFunction, couplingsArray, computables, entanglementMeasures)
-    computableResults = [zeros(length(couplingsArray)) for i in 1:(length(computables) + length(entanglementMeasures))]
-    pbar = Progress(length(couplingsArray))
-    for (i, couplings) in enumerate(couplingsArray)
-        hamiltonian = hamiltonianFunction(basisStates, couplings, false)
-        E, X = diagonalise(basisStates, hamiltonian)
-        for (j, (operatorChunk, indices)) in enumerate(computables)
-            computableResults[j][i] = MatrixElement(X[1], [1 operatorChunk indices], X[1])
-        end
-        for (j, (measure, indices)) in enumerate(entanglementMeasures)
-            computableResults[j + length(computables)][i] = mapToFunction(measure)(X[1], indices)
+    dimension = length(basisStates)
+    operatorMatrix = zeros(dimension, dimension)
+    pbar = Progress(length(basisStates))
+    Threads.@threads for (index, state) in collect(enumerate(basisStates))
+        newState = zeros(length(state))
+        for (couplingStrength, operatorChunk, siteIndices) in operatorList
+            newState[:] = state
+            matrixElement = 1
+            for (siteIndex, operator) in zip(reverse(siteIndices), reverse(operatorChunk))
+                newQubit, factor = TransformQubit(state[siteIndex], operator)
+                newState[siteIndex] = newQubit
+                exchangeSign = operator in ['+', '-'] ? (-1)^sum(state[1:siteIndex]) : 1
+                matrixElement *= factor * exchangeSign
+            end
+            if matrixElement != 0
+                newIndex = findfirst(x->x==newState, basisStates)
+                operatorMatrix[index, newIndex] += matrixElement * couplingStrength
+            end
         end
         next!(pbar)
     end
-    return computableResults
-end
-
-
-    
+    return operatorMatrix
+end    
